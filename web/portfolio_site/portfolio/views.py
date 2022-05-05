bf83ebf73d21312c3901d82fea22732728195d7d
kchihogi <kchihogi@gmail.com>
2022-05-05 22:48:11 +0900

    private works add on index page

--- web/portfolio_site/portfolio/views.py
+++ web/portfolio_site/portfolio/views.py
@@ -1,3 +1,4 @@
+from gc import callbacks
 from django.shortcuts import get_object_or_404, get_list_or_404,render
 
 from .models import Profile, Work
@@ -7,13 +8,7 @@ log = Logger("mylog")
 
 def index(request):
     prof = get_list_or_404(Profile)[-1]
-    works = Work.objects.all()
-
-    log.logger.info("profile title:%s" % prof.title)
-
-    for work in works:
-        log.logger.info("work title:%s" % work.title)
-
-
-    context = {'profile': prof,'works': works}
+    works = Work.objects.filter(private=0).order_by('sort')[:3]
+    pworks = Work.objects.exclude(private=0).order_by('sort')[:3]
+    context = {'profile': prof,'works': works,'private_works': pworks}
     return render(request, 'portfolio/index.html', context)
