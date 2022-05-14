6f2472eda0b22ecbae6a5809621bcf4acf13ae1a
kchihogi <kchihogi@gmail.com>
2022-05-15 07:45:46 +0900

    works pages added

--- web/portfolio_site/portfolio/urls.py
+++ web/portfolio_site/portfolio/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'portfolio'
 urlpatterns = [
-    path('', views.IndexView.index, name='index'),
-    path('work/<int:pk>/', views.WorkView.get_work, name='work'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('works/', views.WorksView.as_view(), name='works'),
+    path('work/<int:pk>/', views.WorkView.as_view(), name='work'),
 ]
--- web/portfolio_site/portfolio/views.py
+++ web/portfolio_site/portfolio/views.py
@@ -1,15 +1,15 @@
 from django.db.models import Prefetch
 from django.shortcuts import get_object_or_404, get_list_or_404,render
-from django.views import generic
+from django.views import View, generic
 import math
 
-from .models import Profile, Work, Work_Language_Skill_RelationShip,Language_Skill
+from .models import Profile, Work, Work_Language_Skill_RelationShip, Work_Library_Skill_Relationship, Work_DevOps_Skill_Relationship
 
 from utils.logger import Logger
 log = Logger("mylog")
 
-class IndexView():
-    def index(request):
+class IndexView(View):
+    def get(self, request):
         prof = get_list_or_404(Profile)[-1]
 
         MAX_WORK = 6
@@ -27,9 +27,23 @@ class IndexView():
 
         context = {'profile': prof,'works': works, 'MAX_WORK' : MAX_WORK}
         return render(request, 'portfolio/index.html', context)
-    
-class WorkView:
-    def get_work(request, pk):
+
+class WorksView(View):
+    def get(self, request):
+        prof = get_list_or_404(Profile)[-1]
+
+        works = []
+
+        lang_prefetch = Prefetch('Lang_Works', queryset=Work_Language_Skill_RelationShip.objects.select_related('Language_Skill').order_by('sort'), to_attr='lang_details')
+        lib_prefetch = Prefetch('Lib_Works', queryset=Work_Library_Skill_Relationship.objects.select_related('Library_Skill').order_by('sort'), to_attr='lib_details')
+        dev_prefetch = Prefetch('Dev_Works', queryset=Work_DevOps_Skill_Relationship.objects.select_related('DevOps_Skill').order_by('sort'), to_attr='dev_details')
+        works = Work.objects.order_by('sort').prefetch_related(lang_prefetch, lib_prefetch, dev_prefetch)
+
+        context = {'profile': prof,'works': works}
+        return render(request, 'portfolio/works.html', context)
+
+class WorkView(View):
+    def get(self, request, pk):
         prof = get_list_or_404(Profile)[-1]
         work = get_object_or_404(Work, pk=pk)
         context = {'profile': prof,'work': work}
