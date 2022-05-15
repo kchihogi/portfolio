7fa2646c73ae031bd32d9b1323c51aeb712c90fc
kchihogi <kchihogi@gmail.com>
2022-05-16 07:57:30 +0900

    impl work details view

--- web/portfolio_site/portfolio/views.py
+++ web/portfolio_site/portfolio/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_object_or_404, get_list_or_404,render
 from django.views import View, generic
 import math
 
-from .models import Profile, Work, Work_Language_Skill_RelationShip, Work_Library_Skill_Relationship, Work_DevOps_Skill_Relationship
+from .models import Profile, Work, Work_Language_Skill_RelationShip, Work_Library_Skill_Relationship, Work_DevOps_Skill_Relationship, Work_Detail
 
 from utils.logger import Logger
 log = Logger("mylog")
@@ -45,6 +45,10 @@ class WorksView(View):
 class WorkView(View):
     def get(self, request, pk):
         prof = get_list_or_404(Profile)[-1]
-        work = get_object_or_404(Work, pk=pk)
+        work_detail_prefetch = Prefetch('Work_Details', queryset=Work_Detail.objects.all(), to_attr='work_details')
+        lang_prefetch = Prefetch('Lang_Works', queryset=Work_Language_Skill_RelationShip.objects.select_related('Language_Skill').order_by('sort'), to_attr='lang_details')
+        lib_prefetch = Prefetch('Lib_Works', queryset=Work_Library_Skill_Relationship.objects.select_related('Library_Skill').order_by('sort'), to_attr='lib_details')
+        dev_prefetch = Prefetch('Dev_Works', queryset=Work_DevOps_Skill_Relationship.objects.select_related('DevOps_Skill').order_by('sort'), to_attr='dev_details')
+        work = get_object_or_404(Work.objects.order_by('sort').prefetch_related(work_detail_prefetch, lang_prefetch, lib_prefetch, dev_prefetch), pk=pk)
         context = {'profile': prof,'work': work}
         return render(request, 'portfolio/work.html', context)
