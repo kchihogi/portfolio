f0e77247612e28fcbd94b4ce60ea5abf1c0c6240
kchihogi <kchihogi@gmail.com>
2022-05-20 08:02:21 +0900

    refactoring custom_data.py

--- web/portfolio_site/initial_data/custom_data1.py
+++ web/portfolio_site/initial_data/custom_data1.py
@@ -1,14 +1,27 @@
+"""This module deletes and inserts test data into the DB.
+
+You might execute master_data.py first.
+"""
 import datetime
 from distutils import dir_util
-from django.utils import timezone
 import glob
 import os
-from portfolio.models import Profile, Social_Network_Service, Icon_Mater, Acknowledgment, Language_Skill, Library_Skill, DevOps_Skill, Work, Work_Detail, Work_Language_Skill_RelationShip ,Work_Library_Skill_Relationship, Work_DevOps_Skill_Relationship
-import portfolio_site.settings as settings
+
+from django.utils import timezone
+
+from portfolio_site import settings
+from portfolio.models import Profile, Social_Network_Service, Icon_Mater, Acknowledgment
+from portfolio.models import Language_Skill, Library_Skill, DevOps_Skill
+from portfolio.models import Work, Work_Detail
+from portfolio.models import Work_Language_Skill_RelationShip
+from portfolio.models import Work_Library_Skill_Relationship
+from portfolio.models import Work_DevOps_Skill_Relationship
+
 from utils.cprint import ColorPrint as CP
 
-def ProfileAdd():
-
+def add_profile():
+    """This copies media fiiles to the media root and inserts a profile record.
+    """
     # mediaディレクトリの掃除
     prof_dir = settings.MEDIA_ROOT + '/profile'
     CP.print('Clena up media profile dir.('+ prof_dir +')',CP.GREEN)
@@ -18,7 +31,7 @@ def ProfileAdd():
             CP.print('Deleted file.('+file+')',CP.YELLOW)
 
     # mediaを復元
-    backup_media = f'initial_data/media/profile'
+    backup_media = 'initial_data/media/profile'
     CP.print('Copy media dir.(FROM:'+ backup_media + '  TO:'+ prof_dir +')',CP.GREEN)
     dir_util.copy_tree(backup_media, prof_dir)
 
@@ -42,17 +55,23 @@ def ProfileAdd():
     for i in range(300):
         intro += str(i%10)
     prof.introduction = intro
-    prof.face_photo = f'profile/face.png'
-    prof.sub_photo = f'profile/sub.png'
+    prof.face_photo = 'profile/face.png'
+    prof.sub_photo = 'profile/sub.png'
     prof.save()
 
     # DBにデータを追加
     CP.print('Add SNS records to DB.',CP.GREEN)
     for icon in Icon_Mater.objects.all():
-        sns = Social_Network_Service(Profile_id=prof.pk, name=icon.name, url='https://'+icon.name+'.com', Icon_Mater_id=icon.pk)
+        sns = Social_Network_Service()
+        sns.Profile_id=prof.pk
+        sns.name=icon.name
+        sns.url='https://'+icon.name+'.com'
+        sns.Icon_Mater_id=icon.pk
         sns.save()
 
-def AcknowledgmentAdd():
+def add_acknowledgment():
+    """This inserts an acknowledgment.
+    """
     # DB データを全削除
     CP.print('Delete all DB Acknowledgment records.',CP.GREEN)
     for ack in Acknowledgment.objects.all():
@@ -69,7 +88,9 @@ def AcknowledgmentAdd():
     ack = Acknowledgment(comments = cmts, enable = 1)
     ack.save()
 
-def Language_SkillAdd():
+def add_language_skills():
+    """This inserts language skills.
+    """
     # DB データを全削除
     CP.print('Delete all DB Language_Skill records.',CP.GREEN)
     for record in Language_Skill.objects.all():
@@ -93,7 +114,9 @@ def Language_SkillAdd():
     record = Language_Skill(name = 'SQL(SQL Server, MySQL)', maturity = 5)
     record.save()
 
-def Library_SkillAdd():
+def add_library_skills():
+    """This inserts libraru skills.
+    """
     # DB データを全削除
     CP.print('Delete all DB Library_Skill records.',CP.GREEN)
     for record in Library_Skill.objects.all():
@@ -109,7 +132,9 @@ def Library_SkillAdd():
     record = Library_Skill(name = '.Net Framework', maturity = 2)
     record.save()
 
-def DevOps_SkillAdd():
+def add_dev_ops_skills():
+    """This insets DevOps skills.
+    """
     # DB データを全削除
     CP.print('Delete all DB DevOps_Skill records.',CP.GREEN)
     for record in DevOps_Skill.objects.all():
@@ -153,41 +178,99 @@ def DevOps_SkillAdd():
     record = DevOps_Skill(name = 'myPHPAdmin', maturity = 2)
     record.save()
 
-def _createWork(work_name, private_work, start, end, languages, libs, DevOps):
-    CP.print('Add Work records to DB.('+work_name+')',CP.GREEN)
-    img = f'works/' + work_name + '.png'
+def _create_work(work_name:str, private_work:int, start:datetime, end:datetime, sort:int=0):
+    """This cretes work with foreign relations.
+
+    Args:
+        work_name (str): a name of work
+        private_work (int): 0 for no private work. 1 for private work.
+        start (datetime): start datetime when a project started.
+        end (datetime): end datetime when a project ended.
+        sort (int, optional): sort number to displaye the web page. Defaults to 0.
+
+    Returns:
+        Work: the model of Work created.
+    """
     desc = ''
     for i in range(300):
         if i%10 == 0 and i !=0:
             desc += '\n'
         else:
             desc += 'Z'
-    work = Work(title=work_name, image=img, private=private_work, description = desc, url='https://dummy_url', repository_url='https://dummy_repourl', sort=0)
+    work = Work()
+    work.title = work_name
+    work.image='works/' + work_name + '.png'
+    work.private=private_work
+    work.description = desc
+    work.url='https://dummy_url'
+    work.repository_url='https://dummy_repourl'
+    work.sort=sort
     work.save()
 
-    desc = '詳細情報'
-    work_detail = Work_Detail(Work_id=work.pk, sub_titile='サブタイトル', start_date=start, end_date=end, processes='工程A,工程B,工程C', detail_description= desc)
+    work_detail = Work_Detail()
+    work_detail.Work_id=work.pk
+    work_detail.sub_titile='サブタイトル'
+    work_detail.start_date=start
+    work_detail.end_date=end
+    work_detail.processes='工程A,工程B,工程C'
+    work_detail.detail_description= '詳細情報'
     work_detail.save()
+    return work
 
-    for record in Language_Skill.objects.all():
-        for language in languages:
-            if record.name == language:
-                relation = Work_Language_Skill_RelationShip(Work_id=work.pk, Language_Skill_id=record.pk, sort=0)
-                relation.save()
+def _add_language_skills(work:Work, languages:list[str]):
+    """This cretes relationship with work and language skills.
 
-    for record in Library_Skill.objects.all():
-        for lib in libs:
-            if record.name == lib:
-                relation = Work_Library_Skill_Relationship(Work_id=work.pk, Library_Skill_id=record.pk, sort=0)
-                relation.save()
+    Args:
+        work (Work): the model of work.
+        languages (list[str]): a list of language name.
+    """
+    if languages is not None:
+        for record in Language_Skill.objects.all():
+            for language in languages:
+                if record.name == language:
+                    relation = Work_Language_Skill_RelationShip()
+                    relation.Work_id=work.pk
+                    relation.Language_Skill_id=record.pk
+                    relation.sort=0
+                    relation.save()
 
-    for record in DevOps_Skill.objects.all():
-        for dev in DevOps:
-            if record.name == dev:
-                relation = Work_DevOps_Skill_Relationship(Work_id=work.pk, DevOps_Skill_id=record.pk, sort=0)
-                relation.save()
+def _add_lib_skills(work:Work, libs:list[str]):
+    """This cretes relationship with work and libs skills.
 
-def WorkAdd():
+    Args:
+        work (Work): the model of work.
+        libs (list[str]): a list of library name.
+    """
+    if libs is not None:
+        for record in Library_Skill.objects.all():
+            for lib in libs:
+                if record.name == lib:
+                    relation = Work_Library_Skill_Relationship()
+                    relation.Work_id=work.pk
+                    relation.Library_Skill_id=record.pk
+                    relation.sort=0
+                    relation.save()
+
+def _add_dev_ops_skills(work:Work, dev_ops:list[str]):
+    """This cretes relationship with work and dev_ops skills.
+
+    Args:
+        work (Work): the model of work.
+        dev_ops (list[str]): a list of dev_ops skill name.
+    """
+    if dev_ops is not None:
+        for record in DevOps_Skill.objects.all():
+            for dev in dev_ops:
+                if record.name == dev:
+                    relation = Work_DevOps_Skill_Relationship()
+                    relation.Work_id=work.pk
+                    relation.DevOps_Skill_id=record.pk
+                    relation.sort=0
+                    relation.save()
+
+def add_works():
+    """This copies media files to media root and inserts records of work.
+    """
 
     # mediaディレクトリの掃除
     work_dir = settings.MEDIA_ROOT + '/works'
@@ -198,7 +281,7 @@ def WorkAdd():
             CP.print('Deleted file.('+file+')',CP.YELLOW)
 
     # mediaを復元
-    backup_media = f'initial_data/media/works'
+    backup_media = 'initial_data/media/works'
     CP.print('Copy media dir.(FROM:'+ backup_media + '  TO:'+ work_dir +')',CP.GREEN)
     dir_util.copy_tree(backup_media, work_dir)
 
@@ -239,79 +322,99 @@ def WorkAdd():
     end = timezone.now()
     languages = ['C++','SQL(SQL Server, MySQL)','Powershell','PHP']
     libs = ['F社標準ライブラリ']
-    DevOps = ['Visual Studio','SQL Server Management Studio', 'Mysql Workbench','SVN', 'Git', 'A5 SQL Mk-2', 'Office', 'Redmine', 'JIRA', 'Jenkins', 'Teams']
-    _createWork(work_name=work_name, private_work=private_work, start=start, end=end,languages=languages,libs=libs,DevOps=DevOps)
-    
+    dev_ops = ['Visual Studio','SQL Server Management Studio', 'Mysql Workbench' \
+                ,'SVN', 'Git', 'A5 SQL Mk-2', 'Office', 'Redmine', 'JIRA', 'Jenkins', 'Teams']
+    work = _create_work(work_name=work_name, private_work=private_work, start=start, end=end)
+    _add_language_skills(work=work, languages=languages)
+    _add_lib_skills(work=work,libs=libs)
+    _add_dev_ops_skills(work=work,dev_ops=dev_ops)
+
     work_name = 'WorkB'
     private_work = 0
     start = timezone.now()
     languages = ['C++','Powershell']
     libs = ['F社標準ライブラリ']
-    DevOps = ['Visual Studio', 'Git', 'Office', 'Redmine', 'JIRA', 'Jenkins', 'Docker']
-    _createWork(work_name=work_name, private_work=private_work, start=start, end=None,languages=languages,libs=libs,DevOps=DevOps)
-    
+    dev_ops = ['Visual Studio', 'Git', 'Office', 'Redmine', 'JIRA', 'Jenkins', 'Docker']
+    work = _create_work(work_name=work_name, private_work=private_work, start=start, end=None)
+    _add_language_skills(work=work, languages=languages)
+    _add_lib_skills(work=work,libs=libs)
+    _add_dev_ops_skills(work=work,dev_ops=dev_ops)
+
     work_name = 'WorkC'
     private_work = 0
     start = timezone.now() + datetime.timedelta(days=-365*2)
     end = timezone.now()
     languages = ['C#','Powershell','Java']
     libs = ['.Net Framework']
-    DevOps = ['Visual Studio','VS Code', 'Skype','SVN', 'Slack', 'Office']
-    _createWork(work_name=work_name, private_work=private_work, start=start, end=end,languages=languages,libs=libs,DevOps=DevOps)
-    
+    dev_ops = ['Visual Studio','VS Code', 'Skype','SVN', 'Slack', 'Office']
+    work = _create_work(work_name=work_name, private_work=private_work, start=start, end=end)
+    _add_language_skills(work=work, languages=languages)
+    _add_lib_skills(work=work,libs=libs)
+    _add_dev_ops_skills(work=work,dev_ops=dev_ops)
+
     work_name = 'WorkD'
     private_work = 0
     start = timezone.now() + datetime.timedelta(days=-365)
     end = timezone.now()
     languages = ['Python']
     libs = ['Django']
-    DevOps = ['VS Code', 'Git', 'Docker']
-    _createWork(work_name=work_name, private_work=private_work, start=start, end=end,languages=languages,libs=libs,DevOps=DevOps)
-    
+    dev_ops = ['VS Code', 'Git', 'Docker']
+    work = _create_work(work_name=work_name, private_work=private_work, start=start, end=end)
+    _add_language_skills(work=work, languages=languages)
+    _add_lib_skills(work=work,libs=libs)
+    _add_dev_ops_skills(work=work,dev_ops=dev_ops)
+
     work_name = 'WorkE'
     private_work = 1
     start = timezone.now() + datetime.timedelta(days=-365)
     end = timezone.now()
     languages = []
     libs = []
-    DevOps = []
-    _createWork(work_name=work_name, private_work=private_work, start=start, end=end,languages=languages,libs=libs,DevOps=DevOps)
-    
+    dev_ops = []
+    work = _create_work(work_name=work_name, private_work=private_work, start=start, end=end)
+    _add_language_skills(work=work, languages=languages)
+    _add_lib_skills(work=work,libs=libs)
+    _add_dev_ops_skills(work=work,dev_ops=dev_ops)
+
     work_name = 'WorkF'
     private_work = 1
     start = timezone.now()
     languages = ['C++','Powershell']
     libs = []
-    DevOps = ['Visual Studio', 'Git', 'Office', 'Redmine', 'JIRA', 'Jenkins', 'Docker']
-    _createWork(work_name=work_name, private_work=private_work, start=start, end=None,languages=languages,libs=libs,DevOps=DevOps)
-    
+    dev_ops = ['Visual Studio', 'Git', 'Office', 'Redmine', 'JIRA', 'Jenkins', 'Docker']
+    work = _create_work(work_name=work_name, private_work=private_work, start=start, end=None)
+    _add_language_skills(work=work, languages=languages)
+    _add_lib_skills(work=work,libs=libs)
+    _add_dev_ops_skills(work=work,dev_ops=dev_ops)
+
     work_name = 'WorkG'
     private_work = 1
     start = timezone.now() + datetime.timedelta(days=-365*2)
     end = timezone.now()
     languages = ['C#','Powershell','Java']
     libs = ['.Net Framework']
-    DevOps = []
-    _createWork(work_name=work_name, private_work=private_work, start=start, end=end,languages=languages,libs=libs,DevOps=DevOps)
-    
+    dev_ops = []
+    work = _create_work(work_name=work_name, private_work=private_work, start=start, end=end)
+    _add_language_skills(work=work, languages=languages)
+    _add_lib_skills(work=work,libs=libs)
+    _add_dev_ops_skills(work=work,dev_ops=dev_ops)
+
     work_name = 'WorkH'
     private_work = 1
     start = timezone.now() + datetime.timedelta(days=-365)
     end = timezone.now()
     languages = []
     libs = ['Django']
-    DevOps = ['VS Code', 'Git', 'Docker']
-    _createWork(work_name=work_name, private_work=private_work, start=start, end=end,languages=languages,libs=libs,DevOps=DevOps)
+    dev_ops = ['VS Code', 'Git', 'Docker']
+    work = _create_work(work_name=work_name, private_work=private_work, start=start, end=end)
+    _add_language_skills(work=work, languages=languages)
+    _add_lib_skills(work=work,libs=libs)
+    _add_dev_ops_skills(work=work,dev_ops=dev_ops)
 
-try:
-    ProfileAdd()
-    AcknowledgmentAdd()
-    Language_SkillAdd()
-    Library_SkillAdd()
-    DevOps_SkillAdd()
-    WorkAdd()
-except Exception as e:
-    CP.print('Exception occurs !!!',CP.RED)
-    print(e)
-except:
-    CP.print('Unhandle Error !!!',CP.RED)
+if  __name__ == "__main__":
+    add_profile()
+    add_acknowledgment()
+    add_language_skills()
+    add_library_skills()
+    add_dev_ops_skills()
+    add_works()
