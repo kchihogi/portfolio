10a9df837914be4042599599a43a916a6a92ce76
kchihogi <kchihogi@gmail.com>
2022-05-23 07:25:12 +0900

    refactoring admin.pu

--- web/portfolio_site/portfolio/admin.py
+++ web/portfolio_site/portfolio/admin.py
@@ -1,11 +1,23 @@
+"""This module set up the admin page of django site.
+"""
 from django.contrib import admin
-from .models import Profile,Social_Network_Service,Icon_Mater,Acknowledgment,Language_Skill, Library_Skill, DevOps_Skill, Work, Work_Detail, Work_Language_Skill_RelationShip, Work_Library_Skill_Relationship, Work_DevOps_Skill_Relationship
+
+from .models import Profile,Social_Network_Service,Icon_Mater,Acknowledgment
+from .models import Language_Skill, Library_Skill, DevOps_Skill
+from .models import Work, Work_Detail
+from .models import Work_Language_Skill_RelationShip
+from .models import Work_Library_Skill_Relationship
+from .models import Work_DevOps_Skill_Relationship
 
 class SNSInline(admin.TabularInline):
+    """An inline input form for SNS.
+    """
     model = Social_Network_Service
     extra = 3
 
 class ProfileAdmin(admin.ModelAdmin):
+    """The setting of Profile.
+    """
     fieldsets = [
         (None,               {'fields': ['title', 'subtitle']}),
         ('Your information', {'fields': ['last_name', 'first_name', 'job', 'introduction']}),
@@ -14,33 +26,44 @@ class ProfileAdmin(admin.ModelAdmin):
     inlines = [SNSInline]
     list_display = ('title', 'last_name', 'first_name')
 
-class Work_DetailInline(admin.TabularInline):
+class WorkDetailInline(admin.TabularInline):
+    """An inline input form for work details.
+    """
     model = Work_Detail
     extra = 1
 
-class Work_Language_Skill_RelationShipInline(admin.TabularInline):
+class WorkLanguageSkillRelationShipInline(admin.TabularInline):
+    """An inline input form for language skills.
+    """
     model = Work_Language_Skill_RelationShip
     extra = 3
     ordering = ("sort",)
 
-class Work_Library_Skill_RelationshipInline(admin.TabularInline):
+class WorkLibrarySkillRelationshipInline(admin.TabularInline):
+    """An inline input form fofr library skills.
+    """
     model = Work_Library_Skill_Relationship
     extra = 3
     ordering = ("sort",)
 
-class Work_DevOps_Skill_RelationshipInline(admin.TabularInline):
+class WorkDevOpsSkillRelationshipInline(admin.TabularInline):
+    """An inline input form for DevOps skills.
+    """
     model = Work_DevOps_Skill_Relationship
     extra = 3
     ordering = ("sort",)
 
 class WorkAdmin(admin.ModelAdmin):
+    """The setting of works.
+    """
     fieldsets = [
         (None,               {'fields': ['title', 'description']}),
         ('URL information', {'fields': ['url' ,'repository_url']}),
         ('Image', {'fields': ['image']}),
         ('other', {'fields': ['private', 'sort']}),
     ]
-    inlines = [Work_DetailInline, Work_Language_Skill_RelationShipInline, Work_Library_Skill_RelationshipInline, Work_DevOps_Skill_RelationshipInline]
+    inlines = [WorkDetailInline, WorkLanguageSkillRelationShipInline, \
+                WorkLibrarySkillRelationshipInline, WorkDevOpsSkillRelationshipInline]
     list_display = ('title', 'private')
     list_filter = ['private']
     search_fields = ['question_text']
