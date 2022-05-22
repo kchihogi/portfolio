2b44941787de95d58666786c21b07042d12e39a3
kchihogi <kchihogi@gmail.com>
2022-05-23 07:29:36 +0900

    refactoring apps.py

--- web/portfolio_site/portfolio/apps.py
+++ web/portfolio_site/portfolio/apps.py
@@ -1,6 +1,10 @@
+"""This module is for an application configurations.
+"""
 from django.apps import AppConfig
 
 
 class PortfolioConfig(AppConfig):
+    """An application configuration.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portfolio'
