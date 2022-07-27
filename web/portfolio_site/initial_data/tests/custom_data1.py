230fdb4346359ddea35d428d0f81cb12440b05b8
k_chihogi <k_chihogi@ekty.co.jp>
2022-07-27 12:36:37 +0900

    add initial data

--- web/portfolio_site/initial_data/tests/custom_data1.py
+++ web/portfolio_site/initial_data/tests/custom_data1.py
@@ -5,6 +5,8 @@ You might execute master_data.py first.
 from initial_data import custom_data_utils,master_data
 
 master_data.add_icon_master()
+master_data.add_mail_setting()
+master_data.add_bcc()
 custom_data_utils.add_profile()
 custom_data_utils.add_acknowledgment()
 custom_data_utils.add_language_skills()
--- web/portfolio_site/initial_data/tests/test_master_data.py
+++ web/portfolio_site/initial_data/tests/test_master_data.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 
 from initial_data import master_data
 from portfolio_site import settings
-from portfolio.models import IconMater
+from portfolio.models import Bcc, IconMater, MailSetting
 
 class MasterDataTest(TestCase):
     """This class is an object to test the master data."""
@@ -21,3 +21,17 @@ class MasterDataTest(TestCase):
         self.assertTrue(os.path.isfile(icon_dir + '/LINE.png'))
         self.assertTrue(os.path.isfile(icon_dir + '/Twitter.png'))
         self.assertEqual(IconMater.objects.all().count(), 4)
+
+    def test_add_mail_setting(self):
+        """Check exitsts of one record.
+        """
+        master_data.add_mail_setting()
+        master_data.add_mail_setting()
+        self.assertEqual(MailSetting.objects.all().count(), 1)
+
+    def test_add_bcc(self):
+        """Check exitsts of 2records.
+        """
+        master_data.add_bcc()
+        master_data.add_bcc()
+        self.assertEqual(Bcc.objects.all().count(), 2)
