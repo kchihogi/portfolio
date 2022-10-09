72237691b48f7a632a0d7bab84a184df687e2b48
kchihogi <kchihogi@gmail.com>
2022-10-09 23:45:01 +0900

    デプロイ時にイメージをプルするように修正

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,5 @@ rsync -acvz --delete ./deploy/ $USER@$HOST:/workspace
 ssh $USER@$HOST "date > /workspace/log.txt"
 ssh $USER@$HOST "docker-compose -v >> /workspace/log.txt"
 ssh $USER@$HOST "cp /env/.env /workspace/.env"
-ssh $USER@$HOST "cd /workspace; docker-compose up -d;"
+ssh $USER@$HOST "cd /workspace; docker-compose pull; docker-compose up -d --force-recreate;"
 ssh $USER@$HOST "date >> /workspace/log.txt"
